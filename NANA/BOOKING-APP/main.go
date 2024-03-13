796fbf9cf97d1e7b38fcbd3e97bd709877b63514
frakhmetshin <forhad97@mail.ru>
2024-03-14 01:58:50 +0300

    Variables and constants

--- NANA/BOOKING-APP/main.go
+++ NANA/BOOKING-APP/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	//fmt.Println("Welcome to our", conferenceName, "booking application") //with whitespaces for Println
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available!\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets to attend!")
+
 }
